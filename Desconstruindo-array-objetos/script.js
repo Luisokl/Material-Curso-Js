20a9b1d2184c1bf9fc4890eba17f80d5a43325f5
LUIS FERNANDO NERES DE MELO <100451426+Luisokl@users.noreply.github.com>
2023-01-16 21:24:02 -0300

    Aula sobre desconstruir arrays e objetos

---
+++ Desconstruindo-array-objetos/script.js
@@ -0,0 +1,35 @@
+let pessoa = {
+    nome: "Luis",
+    sobrenome: "Fernando",
+    empresa: "Innovare",
+    cargo: "analista da Garantia da Qualidade"
+};
+
+// console.log(pessoa.nome);
+// console.log(pessoa.cargo);
+
+// Desconstruindo objeto:
+
+// caso o nome da propriedade do objeto já esta sendo utilizada podemos renomear, como no exemplo com o nome abaixo:
+const { nome:nomePessoa, cargo, empresa, sobrenome } = pessoa; // dessa forma podemos chamar as propridades de qualquer objeto
+
+console.log(nomePessoa);
+console.log(sobrenome);
+console.log(empresa);
+console.log(cargo);
+
+
+// Desconstruindo array:
+
+let nomes = ["Matheus", "Lucas", "Carlos"];
+
+let { 0:matheus, 2:terceiraPessoa } = nomes;  // neste exemplos pegamos através do indice 
+
+console.log(matheus);
+console.log(terceiraPessoa);
+
+// outra maneira de descontruir array:
+
+let [primeironome, segundonome] = nomes;  // neste exemplo pegamos atráves da posição 
+
+console.log(primeironome)
