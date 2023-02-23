31b34ab28a5913b1e6496d10e8b61e73e10de112
LUIS FERNANDO NERES DE MELO <100451426+Luisokl@users.noreply.github.com>
2023-02-22 21:31:22 -0300

    manipulando o scroll

--- Manipulação-scroll/script.js
+++ Manipulação-scroll/script.js
@@ -4,3 +4,13 @@ function subirTela() {
         behavior: "smooth"
     });
 }
+
+function botaoScroll() {
+    if (window.scrollY === 0) {
+        //ocultar botão
+        document.querySelector('.scrollbutton').style.display = 'none';
+    } else {
+        // mostrar botão
+        document.querySelector('.scrollbutton').style.display = 'block';
+    }
+}
